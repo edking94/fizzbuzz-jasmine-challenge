98746df883875c717a4385d6d59e6627dc85f43d
edking94 <kingperformancetherapy@gmail.com>
2020-04-24 13:18:46 +0000

    works perfectly

--- fizzbuzz/spec/fizzSpec.js
+++ fizzbuzz/spec/fizzSpec.js
@@ -1,34 +1,32 @@
-describe("my fizzBuzz function", function() {
+describe("my fizzBuzz fuction", function() {
 
-    beforeEach(function() {
-        fizzBuzz = new fizzBuzz();
+    beforeEach(function(){
+        fizzbuzz = new fizzBuzz();
     });
 
-    describe("Returns number, fizz, buzz or fizzBuzz", function() {
-        
-        it ("should exist", function() {
+    describe("Returns Fizz, Buzz, number of FizzBuzz", function() {
+        it("should exist", function() {
             expect(fizzBuzz).toBeDefined();
         });
 
-        it ("it should return Fizz when called as fizzBuzz(9)", function() {
-            var result = fizzBuzz(9)
-            expect(result).toBe("Fizz");
+        it("should return FizzBuzz when called as fizzBuzz(15)", function() {
+        var result = fizzBuzz(15)
+        expect(result).toBe("FizzBuzz")
         });
 
-        it ("it should return Buzz when called as fizzBuzz(10)", function() {
+        it("should return Fizz when called as fizzBuzz(9)", function() {
+        var result = fizzBuzz(9)
+        expect(result).toBe("Fizz")
+        });
+
+        it("should return Buzz when called as fizzBuzz(10)", function() {
             var result = fizzBuzz(10)
-            expect(result).toBe("Buzz");
+            expect(result).toBe("Buzz")
         });
 
-        it ("it should return FizzBuzz when called as fizzBuzz(15)", function() {
-            var result = fizzBuzz(15)
-            expect(result).toBe("FizzBuzz");
-        });
-
-        it ("it should return 2 when called as fizzBuzz(2)", function() {
+        it("should return 2 when called as fizzBuzz(2)", function() {
             var result = fizzBuzz(2)
-            expect(result).toBe(2);
+            expect(result).toBe(2)
         });
-         });
-          });
-
+    });
+})
